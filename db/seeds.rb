841a3af255b77426bb5c9a2a22a3140a3bf05197
samuel chimombe <brentonchimzy2802@gmail.com>
2024-09-04 16:29:49 +0200

    setup of flatpicker

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,27 +39,27 @@ user5 = User.create!(email: "joe4@doe.com", password: "password", address: "123 
   file = URI.open("https://res.cloudinary.com/dbsjj5ynm/image/upload/v1724965272/samples/ecommerce/car-interior-design.jpg")
   service = Service.new(title: "Driver", description: "A great driver", price: 300, location: "Bermuda")
   service.user = user2
-  service.photo.attach(io: file, filename: "new-image.jpg", content_type: "image/jpeg")
+  service.photo.attach(io: file, filename: "car-interior-design.jpg", content_type: "image/jpeg")
   service.save!
   puts "Service created successfully!"
 
   file = URI.open("https://res.cloudinary.com/dbsjj5ynm/image/upload/v1724965272/samples/people/boy-snow-hoodie.jpg")
   service = Service.new(title: "Nanny", description: "The mother you never had", price: 300, location: "Bermuda")
   service.user = user3
-  service.photo.attach(io: file, filename: "new-image.jpg", content_type: "image/jpeg")
+  service.photo.attach(io: file, filename: "boy-snow-hoodie.jpg", content_type: "image/jpeg")
   service.save!
   puts "Service created successfully!"
 
   file = URI.open("https://res.cloudinary.com/dbsjj5ynm/image/upload/v1724965277/samples/two-ladies.jpg")
   service = Service.new(title: "Erotic Dancer", description: "A dancer to give you new life", price: 300, location: "Bermuda")
   service.user = user4
-  service.photo.attach(io: file, filename: "new-image.jpg", content_type: "image/jpeg")
+  service.photo.attach(io: file, filename: "two-ladies.jpg", content_type: "image/jpeg")
   service.save!
   puts "Service created successfully!"
 
   file = URI.open("https://res.cloudinary.com/dbsjj5ynm/image/upload/v1724965271/samples/people/smiling-man.jpg")
   service = Service.new(title: "Handyman", description: "Great All-round", price: 300, location: "Bermuda")
   service.user = user5
-  service.photo.attach(io: file, filename: "new-image.jpg", content_type: "image/jpeg")
+  service.photo.attach(io: file, filename: "smiling-man.jpg", content_type: "image/jpeg")
   service.save!
   puts "Service created successfully!"
