8f7abe5b742db626ac32deca01d44334c53b2d7a
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-09-04 17:30:02 +0200

    added changes to views, routes and seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!
 
   def dashboard
-    @booking = current_user.bookings
+    @bookings = Booking.all
   end
 end
