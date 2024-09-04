ff5815af76a1cb21c175719292b703375c9d8cc2
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-09-04 14:42:15 +0200

    updatet bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
-  
+
   def index
     @bookings = Booking.all
   end
@@ -10,7 +10,8 @@ class BookingsController < ApplicationController
   end
 
   def new
-    @booking = Booking.new
+    @service = Service.find(params[:service_id])
+    @booking = Booking.new(service: @service)
   end
 
   def edit
@@ -18,9 +19,12 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(bookings_params)
+    @service = Service.find(params[:service_id])
+    @booking = @service.bookings.new(booking_params)
+    @booking.service = @service
+    @booking.user = current_user
     if @booking.save
-      redirect_to @booking, notice: 'Booking created successfully.'
+      redirect_to root_path, notice: 'Booking created successfully.'
     else
       render :new, status: :unprocessable_entity
     end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,5 @@ class Service < ApplicationRecord
   belongs_to :user
   # has_many_attached :photos
   has_one_attached :photo
+  has_many :bookings, dependent: :destroy
 end
