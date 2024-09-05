1ff1bc13514be6f9e3ceb4c063bfa719f53165f6
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-09-05 14:34:34 +0200

    fixed controllers, view, dashboard, changed status string to integer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   def dashboard
     # @bookings = Booking.all
     @services = current_user.services
-    @bookings = Booking.join(:service).where(services: { user_id: current_user.id })
+    @bookings = Booking.joins(:service).where(services: { user_id: current_user.id })
   end
 end
