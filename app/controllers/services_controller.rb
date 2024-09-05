f7f0a7c236e92d03a21dc2a2379b3897cee8c522
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-09-05 16:41:51 +0200

    updated services ctrlr, map_controller.js, _info_window

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,15 +3,15 @@ class ServicesController < ApplicationController
     @services = Service.all
 
     # The `geocoded` scope filters only services with coordinates
-    # @services = Service.geocoded
-    # @markers = @services.geocoded.map do |service|
-    #   {
-    #     lat: service.latitude,
-    #     lng: service.longitude
-    #     info_window_html: render_to_string(partial: "info_window", locals: {service: service})
-    #     marker_html: render_to_string(partial: "marker")
-    #   }
-    # end
+    @services = Service.geocoded
+    @markers = @services.geocoded.map do |service|
+      {
+        lat: service.latitude,
+        lng: service.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {service: service}),
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
   end
 
   def show
